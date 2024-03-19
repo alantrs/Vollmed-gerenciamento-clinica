99fd2e016dd278953f895c7c289e2eac816bb2b5
alantrs <alanaraujotrs@gmail.com>
2024-03-18 22:02:49 -0300

    Validador horario de funcionamento da clinica

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -1,2 +1,19 @@
-package med.voll.api.domain.consulta.validacoes;public class ValidadorHorarioFuncionamentoClinica {
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+
+    public void validarHorario(DadosAgendamentoConsulta dados){
+        Boolean isDomingo = dados.dataHoraConsulta().getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        Boolean antesDaAbertura = dados.dataHoraConsulta().getHour() < 7;
+        Boolean depoisDoFechamento = dados.dataHoraConsulta().getHour() > 18;
+
+         if (isDomingo || antesDaAbertura || depoisDoFechamento){
+             throw new ValidacaoException("Consulta fora de horário da clínica");
+         }
+    }
 }
